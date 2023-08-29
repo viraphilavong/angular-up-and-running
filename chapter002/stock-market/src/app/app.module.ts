86ad37e0fca0e4e95ff224da21a7a7a12c00fda9
viraphilavong <alexander@tech-everyday.com>
2023-08-28 22:42:43 -0500

    ran ng new stock-market ch2

--- chapter002/stock-market/src/app/app.module.ts
+++ chapter002/stock-market/src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StockItemComponent } from './stock/stock-item/stock-item.component';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    StockItemComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
